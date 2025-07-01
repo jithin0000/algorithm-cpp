6d152262c7d6b8c109538d77110d4c6fad9a9157
jithin.babu@guccidigital.com <jithin.babu@guccidigital.com>
2025-07-01 10:23:47 +0530

    updated graph visualization

--- include/VGraph.h
+++ include/VGraph.h
@@ -10,77 +10,65 @@
 
 namespace Vis {
 
+struct Vertex {
+  int id;
+  sf::Vector2f pos;
+};
+
 template <typename GraphType> class GraphViz {
 private:
-  std::unordered_map<int, std::pair<float, float>> positions;
+  std::vector<Vertex> vertices;
   GraphType g;
-  float minX = 1e9, maxX = -1e9, minY = 1e9, maxY = -1e9;
-  float centerX;
-  float centerY;
-  float windowCenterX;
-  float windowCenterY; // half of 600
 
 public:
   using NodeID = typename GraphType::NodeIdType;
   GraphViz(const GraphType &G,
-           const std::unordered_map<int, std::pair<float, float>> poss,
+           const std::unordered_map<int, std::pair<float, float>> positions,
            float width, float height)
-      : g(G), positions(poss) {
+      : g(G) {
     if (G.get_node_count() == 0)
       throw std::runtime_error("Graph is empty");
 
     for (const auto &[id, pos] : positions) {
-      minX = std::min(minX, pos.first);
-      maxX = std::max(maxX, pos.first);
-      minY = std::min(minY, pos.second);
-      maxY = std::max(maxY, pos.second);
+      vertices.push_back({id, sf::Vector2f(pos.first, pos.second)});
     }
-
-    centerX = (minX + maxX) / 2.0;
-    centerY = (minY + maxY) / 2.0;
-    windowCenterX = width / 2;  // half of 800
-    windowCenterY = height / 2; // half of 600
   }
 
 private:
-  void drawIdValueAtCenter(sf::RenderWindow &window, sf::Font &font, int id,
-                           std::pair<float, float> pos) {
+  void drawIdValueAtCenter(sf::RenderWindow &window, sf::Font &font, int id) {
 
+    Vertex v = vertices.at(id);
     sf::Text text(font);
+    sf::FloatRect textBounds = text.getLocalBounds();
+    text.setOrigin(textBounds.getCenter());
     text.setString(std::to_string(id));
     text.setCharacterSize(14);
     text.setFillColor(sf::Color::White);
-
-    sf::FloatRect textRect = text.getLocalBounds();
-    text.setOrigin(textRect.getCenter());
-    text.setPosition({pos.first - centerX + windowCenterX,
-                      pos.second - centerY + windowCenterY - 2});
+    text.setPosition(v.pos);
     window.draw(text);
   }
   void drawVertices(sf::RenderWindow &window, sf::Font &font) {
 
-    for (const auto &[id, pos] : positions) {
+    for (const auto &v : vertices) {
       sf::CircleShape circle(10);
-      circle.setPosition({pos.first - centerX + windowCenterX - 10,
-                          pos.second - centerY + windowCenterY - 10});
+      circle.setOrigin({10.0f, 10.0f});
+      circle.setPosition(v.pos);
       circle.setFillColor(sf::Color::Blue);
       window.draw(circle);
-
-      drawIdValueAtCenter(window, font, id, pos);
+      drawIdValueAtCenter(window, font, v.id);
     }
   }
   void drawEdges(sf::RenderWindow &window) {
 
     for (int i = 0; i < g.get_node_count(); i++) {
       for (int j : g.adj()[i]) {
-        auto start = positions.at(i);
-        auto end = positions.at(j);
+
+        Vertex start = vertices.at(i);
+        Vertex end = vertices.at(j);
 
         std::array<sf::Vertex, 2> line = {
-            sf::Vertex{sf::Vector2f(start.first - centerX + windowCenterX,
-                                    start.second - centerY + windowCenterY)},
-            sf::Vertex{sf::Vector2f(end.first - centerX + windowCenterX,
-                                    end.second - centerY + windowCenterY)},
+            sf::Vertex{start.pos},
+            sf::Vertex{end.pos},
         };
         window.draw(line.data(), line.size(), sf::PrimitiveType::Lines);
       }
@@ -92,78 +80,6 @@ public:
     drawVertices(window, font);
     drawEdges(window);
   }
-};
-
-class PathViz {
-private:
-  std::unordered_map<int, std::pair<float, float>> positions;
-  sf::VertexArray p;
-  int currentSegment = 0;
-  std::vector<int> path;
-  float minX = 1e9, maxX = -1e9, minY = 1e9, maxY = -1e9;
-  float centerX;
-  float centerY;
-  float windowCenterX;
-  float windowCenterY; // half of 600
-
-  float lerp(float a, float b, float t) { return a + t * (b - a); };
-
-public:
-  PathViz(const std::vector<int> &pth,
-          const std::unordered_map<int, std::pair<float, float>> &pos,
-          float width, float height)
-      : positions(pos), path(pth), p(sf::PrimitiveType::LineStrip) {
-
-    for (const auto &[id, pos] : positions) {
-      minX = std::min(minX, pos.first);
-      maxX = std::max(maxX, pos.first);
-      minY = std::min(minY, pos.second);
-      maxY = std::max(maxY, pos.second);
-    }
-
-    centerX = (minX + maxX) / 2.0;
-    centerY = (minY + maxY) / 2.0;
-    windowCenterX = width / 2;  // half of 800
-    windowCenterY = height / 2; // half of 600
-    if (!path.empty()) {
-      auto first = positions[path[0]];
-      p.append(
-          sf::Vertex{sf::Vector2f(first.first - centerX + windowCenterX,
-                                  first.second - centerY + windowCenterY)});
-    }
-  }
-
-  void update(float t, sf::Clock &timer) {
-
-    if (path.size() > 0 && currentSegment < path.size() - 1) {
-      auto u = path[currentSegment];
-      auto v = path[currentSegment + 1];
-
-      sf::Vector2f start =
-          sf::Vector2f(positions.at(u).first - centerX + windowCenterX,
-                       positions.at(u).second - centerY + windowCenterY);
-      sf::Vector2f end =
-          sf::Vector2f(positions.at(v).first - centerX + windowCenterX,
-                       positions.at(v).second - centerY + windowCenterY);
-      if (p.getVertexCount() == currentSegment + 1) {
-        p.append(sf::Vertex{start, sf::Color::Red});
-        p.append(sf::Vertex{end, sf::Color::Red});
-      }
-
-      sf::Vector2f currentPos;
-      currentPos.x = lerp(start.x, end.x, t);
-      currentPos.y = lerp(start.y, end.y, t);
-      p[currentSegment + 1].position = currentPos;
-
-      if (t >= 1.0f) {
-        p[currentSegment + 1].position = end;
-        currentSegment++;
-        timer.restart();
-      }
-    }
-  }
-
-  void drawPath(sf::RenderWindow &window) { window.draw(p); }
 };
 
 } // namespace Vis
